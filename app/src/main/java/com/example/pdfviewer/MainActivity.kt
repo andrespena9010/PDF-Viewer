0eb5c97e13b97ee1b5cf73f54a6ba25486fb6e9d
Andres Orbys <a.pena.9010@gmail.com>
2025-03-11 09:23:26 +0100

    Primer visualizacion sencilla exitosa.

--- app/src/main/java/com/example/pdfviewer/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewer/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import com.example.pdfviewer.data.local.LocalData
 import com.example.pdfviewer.navigation.Navigation
-import com.example.pdfviewer.ui.viewmodel.PViewModel
 
 class MainActivity : ComponentActivity() {
-
-    private val viewModel: PViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
